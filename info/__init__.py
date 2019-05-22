02c360bfaa94c75b771576993f1ffeb98c104df8
lee <476850965@qq.com>
2019-05-22 23:57:43 +0800

    抽取app

--- info/__init__.py
+++ info/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask
-from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from flask import  Flask
-from config import Config
-
+from config import Config,config
+from flask_session import Session
 
 app = Flask(__name__)
 #添加配置
-app.config.from_object(Config)
+app.config.from_object(config['testconfig'])
 #初始化数据库
 db = SQLAlchemy(app)
 #初始化redis对象
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_POST)
+redis_store = StrictRedis(host=config['testconfig'].REDIS_HOST,port=config['testconfig'].REDIS_POST)
 #开启当前项目csrf保护
 CSRFProtect(app)
 #设置session保存指定位置
 Session(app)
+
+
